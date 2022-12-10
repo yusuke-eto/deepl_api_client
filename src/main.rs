a95613dbcb1c736f70a3cf32895a18c2d0b1ac09
yusuke-eto <youyoutt0km0@gmail.com>
2022-12-10 14:02:09 +0900

    deeplにapi requestを行えるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let access_token = env::var("DEEPL_API_ACCESS_TOKEN")
+        .expect("Please set environment variable of DEEPL_API_ACCESS_TOKEN");
+
+    let params = [("text", "good morning"), ("target_lang", "JA")];
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api-free.deepl.com/v2/translate")
+        .form(&params)
+        .header("Authorization", format!("DeepL-Auth-Key {}", access_token))
+        .send()
+        .await
+        .unwrap();
+
+    println!("{:?}", res.text().await.unwrap());
 }
