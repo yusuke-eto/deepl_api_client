c20af63981f3e506e1b3da751ff8416b847403f6
yusuke-eto <youyoutt0km0@gmail.com>
2022-12-10 14:29:45 +0900

    オプション引数を受け取れるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use clap::Parser;
 use std::env;
+
+#[derive(Parser)]
+struct Cli {
+    /// The target text to translate
+    text: String,
+    /// The target language
+    target: String,
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Cli::parse();
     let access_token = env::var("DEEPL_API_ACCESS_TOKEN")
         .expect("Please set environment variable of DEEPL_API_ACCESS_TOKEN");
 
-    let params = [("text", "good morning"), ("target_lang", "JA")];
+    let params = [("text", &args.text), ("target_lang", &args.target)];
     let client = reqwest::Client::new();
     let res = client
         .post("https://api-free.deepl.com/v2/translate")
